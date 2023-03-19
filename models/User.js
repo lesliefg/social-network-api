6cb3ea21325de43cc489da96e9bf34639dddf47d
Leslie Garcia <leslie.garcia2433@gmail.com>
2023-03-19 13:29:53 -0500

    Added models

---
+++ models/User.js
@@ -0,0 +1,49 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      // use REGEX to validate correct email
+      match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thoughts',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+// get total count of friends
+userSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
+
+// create the user model using the user Schema
+const User = model('User', userSchema);
+
+// Export user module
+module.exports = User;
